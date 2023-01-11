6bbdb41e3a8bb863ea418b0d425bd17cc80abba7
NupurUpadhyaya <121895946+NupurUpadhyaya@users.noreply.github.com>
2023-01-11 12:19:15 -0500

    Add files via upload

---
+++ src/EntityOrder.java
@@ -0,0 +1,63 @@
+
+public class EntityOrder {
+		String OrderID;
+	    String AccountEmail;
+	    String PaymentID;
+	    String CartID;
+	    String OrderDate;
+	    String Items;
+	    double TotalCost;
+	    String DeliveryType;
+
+	    public String getOrderID() {
+			return OrderID;
+		}
+		public void setOrderID(String orderID) {
+			OrderID = orderID;
+		}
+		public String getAccountEmail() {
+			return AccountEmail;
+		}
+		public void setAccountEmail(String accountEmail) {
+			AccountEmail = accountEmail;
+		}
+		public String getPaymentID() {
+			return PaymentID;
+		}
+		public void setPaymentID(String paymentID) {
+			PaymentID = paymentID;
+		}
+		public String getCartID() {
+			return CartID;
+		}
+		public void setCartID(String cartID) {
+			CartID = cartID;
+		}
+		public String getOrderDate() {
+			return OrderDate;
+		}
+		public void setOrderDate(String orderDate) {
+			OrderDate = orderDate;
+		}
+		public String getItems() {
+			return Items;
+		}
+		public void setItems(String items) {
+			Items = items;
+		}
+		public double getTotalCost() {
+			return TotalCost;
+		}
+		public void setTotalCost(double totalCost) {
+			TotalCost = totalCost;
+		}
+		public String getDeliveryType() {
+			return DeliveryType;
+		}
+		public void setDeliveryType(String deliveryType) {
+			DeliveryType = deliveryType;
+		}
+		
+	    
+	}
+
