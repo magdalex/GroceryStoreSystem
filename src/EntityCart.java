4e74a4b3b095813f9702494f8f92ee803fe4a493
Remy Bourdon <remybourdon@outlook.com>
2023-01-11 13:12:11 -0500

    toString

--- src/EntityCart.java
+++ src/EntityCart.java
@@ -6,21 +6,43 @@ public class EntityCart {
     private List<Integer> quantities;
     private List<Double> totalCosts;
 
+    EntityCart(String cartID) {
+        this.cartID = cartID;
+    }
+
     public String getCartID() {
         return cartID;
     }
 
-    public void setCartID(String cartID) {
-        this.cartID = cartID;
+    public EntityProduct getProduct(int listPosition) {
+        return products.get(listPosition);
     }
 
-    public void get() {
-        // TODO:
+    public Integer getQuantity(int listPosition) {
+        return quantities.get(listPosition);
+    }
+
+    public Double getTotalCost(int listPosition) {
+        return totalCosts.get(listPosition);
     }
 
     public void add(EntityProduct product, int quantity) {
         products.add(product);
         quantities.add(quantity);
         totalCosts.add(product.getProductPrice() * quantity);
+    }
+
+    @Override
+    public String toString() {
+        double cartTotal = 0;
+        String output = "[cartID] " + cartID;
+        for (int i = 0; i < products.size(); i++) {
+            output += "\n" + products.get(i) + " ";
+            output += ", [Quantity] " + quantities.get(i);
+            output += ", [Total Cost] " + totalCosts.get(i);
+            cartTotal += totalCosts.get(i);
+        }
+        output += "\n[Cart Total] " + cartTotal;
+        return output;
     }
 }
