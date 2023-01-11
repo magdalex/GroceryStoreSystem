7531a272c4b2e38d12886c88021574f9c03fe94b
MagneticReaper <121897307+MagneticReaper@users.noreply.github.com>
2023-01-11 12:03:38 -0500

    Update LoginSystem.java
    1. fixed password check so it only checks the current email for a match instead of all accounts
    2. split login email and login password so that code doesn't keep running after signup, instead it returns to greetings after creating an account.

--- src/LoginSystem.java
+++ src/LoginSystem.java
@@ -3,30 +3,19 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class LoginSystem {
-	//don't specify if email/password is wrong for better security instead just say "something" is wrong.
+	// don't specify if email/password is wrong for better security instead just say
+	// "something" is wrong.
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-
+		// new scanner to be passed to methods
 		Scanner scan = new Scanner(System.in);
 
 		// mock database
-
 		HashMap<String, String> database = Userdata.read();
 
 		greetingPage(scan, database);
-
 		scan.close();
-
-		// SIGN UP PAGE
-
-		// signUp(scan, database);
-
-		// LOG IN PAGE
-
-		// logIn(scan, database);
-
-	} // Main
+	}
 
 	public static void greetingPage(Scanner scan, HashMap<String, String> database) throws IOException {
 		System.out.println("Do you have an existing account already?");
@@ -55,13 +44,15 @@ public class LoginSystem {
 		int count = 1;
 		while (true) {
 			if (database.containsKey(email)) {
+				logInPass(scan, database, email);
 				break;
 			} else {
 				System.out.println("Please enter a valid email or type 1 if you'd like to create an account.");
 				email = scan.next();
 				count++;
 				if (count == 3) {
-					System.out.println("You will now be sent to the Sign-Up page due to multiple invalid email inputs"+'\n');
+					System.out.println(
+							"You will now be sent to the Sign-Up page due to multiple invalid email inputs" + '\n');
 					signUp(scan, database);
 					break;
 				}
@@ -70,15 +61,17 @@ public class LoginSystem {
 					break;
 				}
 			}
-
 		}
+	}
+
+	public static void logInPass(Scanner scan, HashMap<String, String> database, String email) {
 
 		// password input
 		System.out.println("Please enter your password");
 		String password = scan.next();
-		count = 1;
+		int count = 1;
 		while (true) {
-			if (database.containsValue(password)) {
+			if (database.get(email).contentEquals(password)) {
 				break;
 			} else {
 				System.out.println("Please enter a valid password.");
@@ -91,10 +84,8 @@ public class LoginSystem {
 					break;
 				}
 			}
-
 		}
 		System.out.println("You have succesfully logged in!");
-
 	}
 
 	public static void signUp(Scanner scan, HashMap<String, String> database) throws IOException {
@@ -122,7 +113,7 @@ public class LoginSystem {
 		database.put(emailSignUp, passwordSignUp);
 		System.out.println("Welcome! You have succesfully signed up!");
 		Userdata.write(database);
-		System.exit(1);
+		greetingPage(scan, database);
 	}
 
 }
