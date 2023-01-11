0e188f6dd221afc55d917978116428e3faa5ba7d
MagneticReaper <121897307+MagneticReaper@users.noreply.github.com>
2023-01-11 11:03:14 -0500

    Create EntityCategory.java

---
+++ src/EntityCategory.java
@@ -0,0 +1,21 @@
+public class EntityCategory {
+    private String categoryName;
+    private EntityProduct productLink;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public EntityProduct getProductLink() {
+        return productLink;
+    }
+
+    public void setProductLink(EntityProduct productLink) {
+        this.productLink = productLink;
+    }
+
+}
