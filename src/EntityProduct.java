a7cef59d9604234c0ac0b23c3c8068f7d506425f
MagneticReaper <121897307+MagneticReaper@users.noreply.github.com>
2023-01-11 11:16:59 -0500

    Update and rename EntityCategory.java to EntityProduct.java

---
+++ src/EntityProduct.java
@@ -0,0 +1,44 @@
+public class EntityProduct {
+    String productID;
+    String productName;
+    String productDescription;
+    double productPrice;
+    String productCategory;
+    int productAvailability;
+    public String getProductID() {
+        return productID;
+    }
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public String getProductDescription() {
+        return productDescription;
+    }
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+    public double getProductPrice() {
+        return productPrice;
+    }
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+    public String getProductCategory() {
+        return productCategory;
+    }
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+    public int getProductAvailability() {
+        return productAvailability;
+    }
+    public void setProductAvailability(int productAvailability) {
+        this.productAvailability = productAvailability;
+    }
+}
